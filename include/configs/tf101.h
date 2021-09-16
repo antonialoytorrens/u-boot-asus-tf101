93fa56b61836882a0baf5178e7a60db930a7c90b
Svyatoslav Ryhel <clamor95@gmail.com>
2021-09-16 22:58:55 +0300

    tf101: add bootcommand

--- include/configs/tf101.h
+++ include/configs/tf101.h
@@ -13,6 +13,25 @@
 /* High-level configuration options */
 #define CONFIG_TEGRA_BOARD_STRING	"ASUS Transformer"
 
+#define BOARD_EXTRA_ENV_SETTINGS \
+	"bootargs=root=/dev/mmcblk1p2 rw nvmem=128M@384M mem=1024M@0M vmalloc=128M gpt console=tty0 usbcore.oldscheme_first=1\0" \
+	"kernel_addr_r=0x12000000\0" \
+	"dtb_addr_r=0x13000000\0" \
+	"ramdisk_addr_r=0x14000000\0" \
+	"kernel_file=zImage\0" \
+	"dtb_file=tegra20-asus-tf101.dtb\0" \
+	"ramdisk_file=uInitrd\0"
+
+/* Boot from fat on uSD */
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND \
+	"echo Loading DTB;" \
+	"fatload mmc 1:1 ${dtb_addr_r} ${dtb_file};" \
+	"echo Loading Kernel;" \
+	"fatload mmc 1:1 ${kernel_addr_r} ${kernel_file};" \
+	"echo Booting Kernel;" \
+	"bootz ${kernel_addr_r} - ${dtb_addr_r}"
+
 /* Board-specific serial config */
 #define CONFIG_TEGRA_ENABLE_UARTD
 #define CONFIG_SYS_NS16550_COM1		NV_PA_APB_UARTD_BASE
