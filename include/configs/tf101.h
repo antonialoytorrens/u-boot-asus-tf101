f07f12e80d204785f2a6e58821f03a4d984543d0
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2021-09-22 23:38:39 +0000

    Removed ENV settings

--- include/configs/tf101.h
+++ include/configs/tf101.h
@@ -13,50 +13,6 @@
 /* High-level configuration options */
 #define CONFIG_TEGRA_BOARD_STRING	"ASUS Eee Pad Transformer TF101"
 
-/*
-* Defined parameters by tegra20-common.h:
-*
-* "scriptaddr=0x10000000\0" \
-* "pxefile_addr_r=0x10100000\0" \
-* "kernel_addr_r=" __stringify(CONFIG_LOADADDR) "\0" \
-* "fdtfile=" FDTFILE "\0" \
-* "fdt_addr_r=0x03000000\0" \
-* "ramdisk_addr_r=0x03100000\0"
-*
-*/
-#define BOARD_EXTRA_ENV_SETTINGS \
-	"loadbootscript=load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr\0" \
-	"loadkernel=load ${dev_type} ${mmcdev}:${mmcpart} ${kernel_addr_r} ${kernelname}\0" \
-	"loadinitrd=load ${dev_type} ${mmcdev}:${mmcpart} ${ramdisk_addr_r} ${initrdname}\0" \
-	"loaddtb=load ${dev_type} ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
-	"check_ramdisk=" \
-		"if run loadinitrd; then " \
-			"setenv initrd_addr ${ramdisk_addr_r};" \
-		"else " \
-			"setenv initrd_addr -;" \
-		"fi; \0" \
-	"check_dtb=" \
-		"if run loaddtb; then " \
-			"setenv fdt_addr ${fdt_addr_r};" \
-		"else " \
-			"setenv fdt_addr;" \
-		"fi; \0" \
-	"kernel_args=" \
-		"setenv bootargs rw nvmem=128M@384M mem=1024M@0M vmalloc=128M gpt" \
-		" usbcore.oldscheme_first=1 lp0_vec=8192@0x1fbed000 ${console} ${opts}\0" \
-	"boot_script=" \
-		"run loadbootscript;" \
-		"source ${scriptaddr}\0" \
-	"console=tty0\0" \
-	"mmcdev=1\0" \
-	"mmcpart=1\0" \
-	"dev_type=mmc\0" \
-	"kernelname=zImage\0" \
-	"initrdname=initramfs\0" \
-	"scriptaddr=0x12000000\0" \
-	"kernel_addr_r=0x13000000\0" \
-	"fdt_addr_r=0x14000000\0"
-
 /* Configurate from PER on eMMC */
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
@@ -64,22 +20,17 @@
 		"setenv mmcdev 0;" \
 		"setenv mmcpart 1;" \
 		"setenv dev_type usb;" \
-		"run boot_script; " \
+		"load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr;" \
 	"elif load usb 1:1 ${scriptaddr} boot.scr; then " \
 		"setenv mmcdev 0;" \
 		"setenv mmcpart 1;" \
 		"setenv dev_type usb;" \
-		"run boot_script; " \
+		"load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr" \
 	"elif load mmc 1:1 ${scriptaddr} boot.scr; then " \
 		"setenv mmcdev 1;" \
 		"setenv mmcpart 1;" \
 		"setenv dev_type mmc;" \
-		"run boot_script; " \
-	"elif load mmc 0:5 ${scriptaddr} boot.scr; then " \
-		"setenv mmcdev 1;" \
-		"setenv mmcpart 1;" \
-		"setenv dev_type mmc;" \
-		"run boot_script; " \
+		"load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr" \
 	"fi;\0"
 
 /* Board-specific serial config */
