70443037d9ff248be4e25cedce76b03677944d3d
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2021-09-23 21:17:32 +0000

    Added boot sequence order

--- include/configs/tf101.h
+++ include/configs/tf101.h
@@ -14,10 +14,34 @@
 /* High-level configuration options */
 #define CONFIG_TEGRA_BOARD_STRING	"ASUS Transformer"
 
-/* Configurate from PER on eMMC */
+/*
+*  Boot sequence order:
+*  1. USB (checks the two ports on TF101 dock)
+*  2. MicroSD
+*  3. eMMC
+*
+*  NOTE: sleep 5 seconds before booting, so the user can read
+*/
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
-	"if load mmc 1:1 ${scriptaddr} uboot-script.cmd; then " \
+	"echo Boot order: 1. USB, 2. MicroSD, 3. eMMC;" \
+	"echo NOTICE: If you have an SD Card attached to the TF1O1 dock, please disconnect it and reboot.;" \
+	"sleep 5;" \
+	"echo Searching for uboot-script.cmd...;" \
+	"if load usb 0:1 ${scriptaddr} uboot-script.cmd; then " \
+		"echo uboot-script.cmd found in first USB.;" \
+		"echo Running uboot-script.cmd...;" \
+		"sleep 5;" \
+		"env import -t -r ${scriptaddr} ${filesize};" \
+	"elif load usb 1:1 ${scriptaddr} uboot-script.cmd; then " \
+		"echo uboot-script.cmd found in second USB.;" \
+		"echo Running uboot-script.cmd...;" \
+		"sleep 5;" \
+		"env import -t -r ${scriptaddr} ${filesize};" \
+	"elif load mmc 1:1 ${scriptaddr} uboot-script.cmd; then " \
+		"echo uboot-script.cmd found in MicroSD card.;" \
+		"echo Running uboot-script.cmd...;" \
+		"sleep 5;" \
 		"env import -t -r ${scriptaddr} ${filesize};" \
 	"else " \
 		"echo Boot Configuration NOT FOUND!; " \
