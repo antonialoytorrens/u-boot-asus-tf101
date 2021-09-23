0ed96323e4da821bf9d5150ef961ae5707d7c3ce
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2021-09-23 20:52:15 +0000

    Set up working minimal TF101 uboot

--- include/configs/tf101.h
+++ include/configs/tf101.h
@@ -1,3 +1,4 @@
+
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
  *  (C) Copyright 2010,2011
@@ -11,27 +12,16 @@
 #include "tegra20-common.h"
 
 /* High-level configuration options */
-#define CONFIG_TEGRA_BOARD_STRING	"ASUS Eee Pad Transformer TF101"
+#define CONFIG_TEGRA_BOARD_STRING	"ASUS Transformer"
 
 /* Configurate from PER on eMMC */
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
-	"if load usb 0:1 ${scriptaddr} boot.scr; then " \
-		"setenv mmcdev 0;" \
-		"setenv mmcpart 1;" \
-		"setenv dev_type usb;" \
-		"load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr;" \
-	"elif load usb 1:1 ${scriptaddr} boot.scr; then " \
-		"setenv mmcdev 0;" \
-		"setenv mmcpart 1;" \
-		"setenv dev_type usb;" \
-		"load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr" \
-	"elif load mmc 1:1 ${scriptaddr} boot.scr; then " \
-		"setenv mmcdev 1;" \
-		"setenv mmcpart 1;" \
-		"setenv dev_type mmc;" \
-		"load ${dev_type} ${mmcdev}:${mmcpart} ${scriptaddr} boot.scr" \
-	"fi;\0"
+	"if load mmc 1:1 ${scriptaddr} uboot-script.cmd; then " \
+		"env import -t -r ${scriptaddr} ${filesize};" \
+	"else " \
+		"echo Boot Configuration NOT FOUND!; " \
+	"fi;"
 
 /* Board-specific serial config */
 #define CONFIG_TEGRA_ENABLE_UARTD
