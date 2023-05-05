595d7dff1f2f7f591c30321c24a49327b9f3f3ea
Svyatoslav Ryhel <clamor95@gmail.com>
2023-05-05 21:20:06 +0300

    asus: tf-t20: remove unneeded stuff
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- board/asus/transformer-t20/transformer-t20.c
+++ board/asus/transformer-t20/transformer-t20.c
@@ -84,65 +84,6 @@ void pin_mux_mmc(void)
 {
 	funcmux_select(PERIPH_ID_SDMMC4, FUNCMUX_SDMMC4_ATB_GMA_GME_8_BIT);
 	funcmux_select(PERIPH_ID_SDMMC3, FUNCMUX_SDMMC3_SDB_4BIT);
-
-	/* For SD: GPIO PI5, PH1, PT3 */
-	pinmux_tristate_disable(PMUX_PINGRP_ATC);
-	pinmux_tristate_disable(PMUX_PINGRP_ATD);
-
-	pinmux_set_pullupdown(PMUX_PINGRP_DTB, PMUX_PULL_DOWN);
-	pinmux_tristate_disable(PMUX_PINGRP_DTB);
-
-	/* For EMMC: GPIO PI6 */
-	pinmux_tristate_disable(PMUX_PINGRP_ATA);
-}
-#endif
-
-#ifdef CONFIG_USB_EHCI_TEGRA
-static void tps6586x_usb_voltage_init(void)
-{
-	struct udevice *dev;
-	uchar data_buffer[1];
-	int ret;
-
-	ret = i2c_get_chip_for_busnum(0, TPS6586X_I2C_ADDRESS, 1, &dev);
-	if (ret) {
-		log_debug("cannot find PMIC I2C chip\n");
-		return;
-	}
-
-	ret = dm_i2c_read(dev, TPS6586X_SUPPLY_V4, data_buffer, 1);
-	if (ret)
-		log_debug("supply_v4 read failed: %d\n", ret);
-
-	data_buffer[0] |= VLDO3_3v3;
-
-	/* Set LDO3 voltage to 3.3v */
-	ret = dm_i2c_write(dev, TPS6586X_SUPPLY_V4, data_buffer, 1);
-	if (ret)
-		log_debug("avdd_usb set failed: %d\n", ret);
-
-	ret = dm_i2c_read(dev, TPS6586X_SUPPLY_ENC, data_buffer, 1);
-	if (ret)
-		log_debug("supply_enc read failed: %d\n", ret);
-
-	data_buffer[0] |= LDO3_ONC;
-
-	/* Enable LDO3 */
-	ret = dm_i2c_write(dev, TPS6586X_SUPPLY_ENC, data_buffer, 1);
-	if (ret)
-		log_debug("avdd_usb enable failed: %d\n", ret);
-}
-
-void pin_mux_usb(void)
-{
-	/* For USB0's VBus GPIO PD0 */
-	pinmux_tristate_disable(PMUX_PINGRP_SLXK);
-
-	/* For USB2's VBus GPIO PD3 */
-	pinmux_tristate_disable(PMUX_PINGRP_SLXC);
-
-	/* Set PMIC supplies */
-	tps6586x_usb_voltage_init();
 }
 #endif
 
